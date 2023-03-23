94f50cd471c88a4b66f7da565e5f2d70dc3ae3a8
Musa Nyembe <musa.nyembe88@gmail.com>
2023-03-23 05:07:26 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point.
@@ -7,14 +8,7 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar("_putchar");
 	_putchar('\n');
 
 	return (0);
